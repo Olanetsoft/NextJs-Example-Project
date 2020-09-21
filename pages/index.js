2472446be119b7c4853e4d01e4fb455887425e0c
olanetsoft <heedris2olubisi@gmail.com>
2020-09-21 14:13:02 +0100

    coindesk api consumed

--- pages/index.js
+++ pages/index.js
@@ -1,14 +1,27 @@
+import fetch from 'isomorphic-unfetch';
 import Layout from '../components/layout';
 
-const Index = () => (
+import Prices from '../components/prices';
+
+const Index = (props) => (
     <Layout>
         <div>
             <h1>
-                Hello Next.js
+                Hello Users
             </h1>
+            <p>Check current Bitcoin Rate</p>
+            <Prices bpi={props.bpi}/>
+            {/* {props.bpi.time.updated} */}
         </div>
     </Layout>
 );
 
+Index.getInitialProps = async function () {
+    const res = await fetch('https://api.coindesk.com/v1/bpi/currentprice.json')
+    const data = await res.json();
+    return {
+        bpi: data.bpi
+    };
+}
 
 export default Index;
